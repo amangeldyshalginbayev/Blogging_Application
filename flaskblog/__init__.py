c19b1dc4a691a87572ac9d3195f44ce10e1b34cc
Amangeldy Shalginbayev <a.shalginbayev@yandex.com>
2020-05-25 17:31:48 +0300

    Implemented password reset via email

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,8 +1,13 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
+
+os.environ['EMAIL_USER'] = 'put login here'
+os.environ['EMAIL_PASS'] = 'put password here'
 
 
 app = Flask(__name__)
@@ -13,5 +18,12 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
+
 
 from flaskblog import routes
