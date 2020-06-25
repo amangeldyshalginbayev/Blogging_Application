3c27db48acde4dbbe0c3e73ecca79154e209775a
Amangeldy Shalginbayev <a.shalginbayev@yandex.com>
2020-06-25 18:59:58 +0300

    fixed config bug

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -11,14 +11,13 @@ class Config:
     """
     SECRET_KEY = os.environ.get("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get(
-        "SQLALCHEMY_TRACK_MODIFICATIONS")
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get("SQLALCHEMY_TRACK_MODIFICATIONS")
     MAIL_SERVER = os.environ.get("MAIL_SERVER")
     MAIL_PORT = os.environ.get("MAIL_PORT")
     MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS")
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER")
+    MAIL_DEFAULT_SENDER = ('Flask Blog', 'flaskblog-noreply@demo.com')
     MESSENTE_API_USERNAME = os.environ.get("MESSENTE_API_USERNAME")
     MESSENTE_API_PASSWORD = os.environ.get("MESSENTE_API_PASSWORD")
 
