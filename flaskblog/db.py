a5b824205eedd581d60587485e9c35279dcf0781
Amangeldy Shalginbayev <a.shalginbayev@yandex.com>
2020-06-25 18:19:11 +0300

    fixed bug

--- flaskblog/db.py
+++ flaskblog/db.py
@@ -6,9 +6,9 @@ from flaskblog import create_app, db
 @click.command('init-db')
 @with_appcontext
 def init_db_command():
-	"""This function is used only once from command line for initial
-	database setup when configuring the app in new environment
-	"""
+    """This function is used only once from command line for initial
+    database setup when configuring the app in new environment
+    """
     with create_app().app_context():
-    	db.create_all()
+        db.create_all()
     click.echo('Initialized the database.')
