5845e1314cd00b3d002dca148d276112c31266bf
Amangeldy Shalginbayev <a.shalginbayev@yandex.com>
2020-06-25 18:34:17 +0300

    fixed config bug

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -9,19 +9,18 @@ class Config:
     environment variables. 'development_config.cfg' file is not pushed to
     production and used only for local development.
     """
-
-
-SECRET_KEY = os.environ.get("SECRET_KEY")
-SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get("SQLALCHEMY_TRACK_MODIFICATIONS")
-MAIL_SERVER = os.environ.get("MAIL_SERVER")
-MAIL_PORT = os.environ.get("MAIL_PORT")
-MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS")
-MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER")
-MESSENTE_API_USERNAME = os.environ.get("MESSENTE_API_USERNAME")
-MESSENTE_API_PASSWORD = os.environ.get("MESSENTE_API_PASSWORD")
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get(
+        "SQLALCHEMY_TRACK_MODIFICATIONS")
+    MAIL_SERVER = os.environ.get("MAIL_SERVER")
+    MAIL_PORT = os.environ.get("MAIL_PORT")
+    MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS")
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER")
+    MESSENTE_API_USERNAME = os.environ.get("MESSENTE_API_USERNAME")
+    MESSENTE_API_PASSWORD = os.environ.get("MESSENTE_API_PASSWORD")
 
 
 class TestConfig(Config):
